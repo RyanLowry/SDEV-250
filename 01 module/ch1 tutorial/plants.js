967cf14f32ff13bf5083e5dd009f704872adc15d
RyanLowry <ryanlowry9@gmail.com>
2021-02-04 11:18:15 -0500

    Add ch01 files

---
+++ 01 module/ch1 tutorial/plants.js
@@ -0,0 +1,16 @@
+/*
+JavaScript 6th Edition
+Chapter 1
+Chapter case
+
+Tinley  Xeriscapes
+Author: Ryan Lowry
+Date:   1/21/2021
+
+Filename: plants.js
+*/
+
+//define variables containing img src values
+var blanket = "images/blanket.jpg";
+var bluestem = "images/bluestem.jpg";
+var rugosa = "images/rugosa.jpg";
